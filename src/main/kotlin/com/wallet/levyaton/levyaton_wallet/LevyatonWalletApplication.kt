ba316ecc68086516a9b0ebc6acfda30cb4855e52
czech <czech98@gmail.com>
2024-11-15 14:34:04 +0100

    feat: Init

---
+++ src/main/kotlin/com/wallet/levyaton/levyaton_wallet/LevyatonWalletApplication.kt
@@ -0,0 +1,11 @@
+package com.wallet.levyaton.levyaton_wallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LevyatonWalletApplication
+
+fun main(args: Array<String>) {
+	runApplication<LevyatonWalletApplication>(*args)
+}
---
+++ src/test/kotlin/com/wallet/levyaton/levyaton_wallet/LevyatonWalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wallet.levyaton.levyaton_wallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LevyatonWalletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
