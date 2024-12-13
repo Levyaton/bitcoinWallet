b21e9d0058f5fd2f510e37d64aef021e548b668f
czech <czech98@gmail.com>
2024-12-13 15:08:43 +0100

    feat: Implement Registration and login endpoints

--- src/main/kotlin/com/wallet/levyaton/levyaton_wallet/LevyatonWalletApplication.kt
+++ src/main/kotlin/com/wallet/levyaton/levyaton_wallet/LevyatonWalletApplication.kt
@@ -1,11 +1,22 @@
 package com.wallet.levyaton.levyaton_wallet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class LevyatonWalletApplication
 
 fun main(args: Array<String>) {
 	runApplication<LevyatonWalletApplication>(*args)
+}
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**").allowedMethods("*")
+	}
 }
